0765002f4197f4c8e3a290b61a9026227295f9d3
ketanBisht <ketan.bisht04@gmail.com>
2026-02-20 00:50:19 +0530

    extension

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Required for browser extensions to load assets correctly
   plugins: [
     react(),
     nodePolyfills(),
